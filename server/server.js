5a4031feff8fe7b374aaa5d070ab4b225b261579
Jacob Goldfine <jacobjgoldfine@gmail.com>
2021-11-09 19:01:45 -0600

    fixed server error?

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ app.get("*", (req, res) => {
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
